9f3458831146c82348c0a5304ea5edeea0848cdf
Narumikazuchi <ganzel.davy@swschwedt.de>
2024-02-13 10:00:03 +0100

    Changed from Blazor Server to Blazor WebApp for interactivity.

---
+++ Narumikazuchi.CreativeGallery/Infrastructure/Routes.cs
@@ -0,0 +1,7 @@
+﻿namespace Narumikazuchi.CreativeGallery.Infrastructure;
+
+static public class Routes
+{
+    public const String LANDING_PAGE = "/";
+    public const String ERROR_PAGE = "/Error";
+}
--- Narumikazuchi.CreativeGallery/Program.cs
+++ Narumikazuchi.CreativeGallery/Program.cs
@@ -1,26 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Narumikazuchi.CreativeGallery.Core;
+using Narumikazuchi.CreativeGallery.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args: args);
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment() is false)
 {
+    app.UseExceptionHandler(errorHandlingPath: Routes.ERROR_PAGE,
+                            createScopeForErrors: true);
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
 
 app.Run();
